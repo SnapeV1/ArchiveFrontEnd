50465d716a75cc4c4dbea2b032c38c9a8d156e96
SnapeV1 <hamada_amine@yahoo.com>
2024-06-09 22:33:09 +0100

    search

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../Services/service.service';
 import { StagiareModule } from '../models/stagiare/stagiare.module';
 
@@ -8,22 +8,37 @@ import { StagiareModule } from '../models/stagiare/stagiare.module';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit{
 
   stagiaires: StagiareModule[] = [];
+  searchQuery: string = '';
+  filteredStagiaires: StagiareModule[] = [];
 
   constructor(private http: HttpClient,private service:ServiceService) { }
 
 
   ngOnInit(): void {
   this.getStagiaires();
+
   }
-  getStagiaires(){
+  getStagiaires(): void {
     this.service.getStagiaires().subscribe(
-      data => this.stagiaires = data,
+      data => {
+        this.stagiaires = data;
+        this.updateFilteredStagiaires();  
+      },
       error => console.error('There was an error!', error)
     );
   }
 
-
+  updateFilteredStagiaires(): void {
+    if (this.searchQuery) {
+      this.filteredStagiaires = this.stagiaires.filter(stagiaire =>
+        stagiaire.identifiant.includes(this.searchQuery)
+      );
+    } else {
+      this.filteredStagiaires = this.stagiaires;
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
